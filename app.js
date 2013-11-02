821e00bbc7b87a233d6227a29db20ee31dc576a1
alexandrejablon <alexandre.jablon@gmail.com>
2013-11-02 20:55:37 +0100

    remove the bodyParser middleware and use only the json and urlencode middleware, add some whitespaces before and after method brackets, remove the navbar shadow, decrease the main div and 404 div shadow, use sans font for the 404 content instead of oswald, add an uppercase letter to the first word of the 404 content

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-var express = require('express');
-var resume = require('./resume.js');
-var _ = require('underscore');
-var app = express();
+var express = require ( 'express' );
+var resume = require ( './resume.js' );
+var _ = require ( 'underscore' );
+var app = express ();
 
-app.use('/static/css', express.static(__dirname + '/static/css'));
-app.use('/static/img', express.static(__dirname + '/static/img'));
-app.use('/static/js', express.static(__dirname + '/static/js'));
-app.use('/static/fonts', express.static(__dirname + '/static/fonts'));
-app.use('/static/libs', express.static(__dirname + '/static/libs'));
+app.use ( express.json () );
+app.use ( express.urlencoded () );
 
-app.use(express.bodyParser());
+app.use ( '/static/css', express.static ( __dirname + '/static/css' ) );
+app.use ( '/static/img', express.static ( __dirname + '/static/img' ) );
+app.use ( '/static/js', express.static ( __dirname + '/static/js' ) );
+app.use ( '/static/fonts', express.static ( __dirname + '/static/fonts' ) );
+app.use ( '/static/libs', express.static ( __dirname + '/static/libs' ) );
 
-app.get ('/', resume.render);
+app.get ( '/', resume.render );
 
-app.get ('/get_resume', resume.get_resume);
+app.get ( '/get_resume', resume.get_resume );
 
-app.post ('/blob', resume.blob);
+app.post ( '/blob', resume.blob );
 
-app.all ('/robots.txt', resume.robots);
+app.all ( '/robots.txt', resume.robots );
 
 app.get ( '/linkedin', resume.linkedin );
 
-app.all ('*', resume.fourofour);
+app.all ( '*', resume.fourofour );
 
-app.listen(8000);
-console.log('Listening on port 8000');
+app.listen ( 8000 );
+console.log ( 'Listening on port 8000' );
