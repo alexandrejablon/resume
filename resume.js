30e04462c83d4740755f69c83059008a7b8c1d3b
alexandrejablon <alexandre.jablon@gmail.com>
2013-11-20 08:57:11 +0100

    remove useless try...except

--- resume.js
+++ resume.js
@@ -24,49 +24,45 @@ exports.get_resume = function (req, res) {
 };
 
 exports.blob = function (req, res) {
-    try {
-	// I know md5 is crap, but it's good enough for this kind of usage
-	var mdsum = crypto.createHash('md5');
-	var ipHash = mdsum.digest('ascii');
-	client.set(ipHash, req.ip, 'EX', 60, 'NX', function (redisErr, redisRes) {
-	    if ( redisErr ) {
-		res.send ( { 'status': 0 } );
+    // I know md5 is crap, but it's good enough for this kind of usage
+    var mdsum = crypto.createHash('md5');
+    var ipHash = mdsum.digest('ascii');
+    client.set(ipHash, req.ip, 'EX', 60, 'NX', function (redisErr, redisRes) {
+	if ( redisErr ) {
+	    res.send ( { 'status': 0 } );
+	} else {
+	    if ( redisRes === null ) {
+		res.send ( { 'status': 3 } );
 	    } else {
-		if ( redisRes === null ) {
-		    res.send ( { 'status': 3 } );
-		} else {
-		    if ( req.body.blobFrom !== "" && req.body.blobContent !== "" ) {
-			var smtpTransport = nodemailer.createTransport ("SMTP", {
-			    service: "Gmail",
-			    auth: {
-				user: "alexandre.jablon.resume@gmail.com",
-				pass: "kOzTqDvLiXlo7bmaKm"
-			    }
-			});
-
-			var mailOptions = {
-			    from: "Resume Alexandre Jablon <resume@jablon.me>",
-			    to: "alexandre.jablon+resume@gmail.com",
-			    subject: "Someone has sent you something from your resume",
-			    html: "<b>From:</b> " + req.body.blobFrom + "<br/><br/><b>Content:</b> <br/>" + req.body.blobContent
+		if ( req.body.blobFrom !== "" && req.body.blobContent !== "" ) {
+		    var smtpTransport = nodemailer.createTransport ("SMTP", {
+			service: "Gmail",
+			auth: {
+			    user: "alexandre.jablon.resume@gmail.com",
+			    pass: "kOzTqDvLiXlo7bmaKm"
 			}
+		    });
 
-			smtpTransport.sendMail (mailOptions, function (error, response) {
-			    if ( error ) {
-				res.send ( { 'status': 0 } );
-			    } else {
-				res.send ( { 'status': 1 } );
-			    }
-			});
-		    } else {
-			res.send ( {'status': 2} );
+		    var mailOptions = {
+			from: "Resume Alexandre Jablon <resume@jablon.me>",
+			to: "alexandre.jablon+resume@gmail.com",
+			subject: "Someone has sent you something from your resume",
+			html: "<b>From:</b> " + req.body.blobFrom + "<br/><br/><b>Content:</b> <br/>" + req.body.blobContent
 		    }
+
+		    smtpTransport.sendMail (mailOptions, function (error, response) {
+			if ( error ) {
+			    res.send ( { 'status': 0 } );
+			} else {
+			    res.send ( { 'status': 1 } );
+			}
+		    });
+		} else {
+		    res.send ( {'status': 2} );
 		}
 	    }
-	});
-    } catch ( e ) {
-	res.send ( { 'status': 0 } );
-    }
+	}
+    });
 };
 
 exports.robots = function (req, res) {
