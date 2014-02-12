b0cd21481b5bce81b08022f0d29bba5ce92f8464
alexandrejablon <alexandre.jablon@gmail.com>
2014-02-12 13:10:27 +0100

    remove useless spaces

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-var express = require ( 'express' );
-var resume = require ( './resume.js' );
-var _ = require ( 'underscore' );
+var express = require ('express');
+var resume = require ('./resume.js');
+var _ = require ('underscore');
 var app = express ();
 
-app.use ( express.json () );
-app.use ( express.urlencoded () );
+app.use (express.json ());
+app.use (express.urlencoded ());
 
-app.use ( '/static/css', express.static ( __dirname + '/static/css' ) );
-app.use ( '/static/img', express.static ( __dirname + '/static/img' ) );
-app.use ( '/static/js', express.static ( __dirname + '/static/js' ) );
-app.use ( '/static/fonts', express.static ( __dirname + '/static/fonts' ) );
-app.use ( '/static/libs', express.static ( __dirname + '/static/libs' ) );
+app.use ('/static/css', express.static (__dirname + '/static/css'));
+app.use ('/static/img', express.static (__dirname + '/static/img'));
+app.use ('/static/js', express.static (__dirname + '/static/js'));
+app.use ('/static/fonts', express.static (__dirname + '/static/fonts'));
+app.use ('/static/libs', express.static (__dirname + '/static/libs'));
 
 app.use (app.router);
 app.use (function (err, req, res, next) {
@@ -21,17 +21,17 @@ app.use (function (err, req, res, next) {
   res.send (500, pub_error);
 });
 
-app.get ( '/', resume.render );
+app.get ('/', resume.render);
 
-app.get ( '/get_resume', resume.get_resume );
+app.get ('/get_resume', resume.get_resume);
 
-app.post ( '/blob', resume.blob );
+app.post ('/blob', resume.blob);
 
-app.get ( '/robots.txt', resume.robots );
+app.get ('/robots.txt', resume.robots);
 
-app.get ( '/linkedin', resume.linkedin );
+app.get ('/linkedin', resume.linkedin);
 
-app.get ( '*', resume.fourofour );
+app.get ('*', resume.fourofour);
 
-app.listen ( 8000 );
-console.log ( 'Listening on port 8000' );
+app.listen (8000);
+console.log ('Listening on port 8000');
