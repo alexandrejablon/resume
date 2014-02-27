641a5ff2cb50342dd63f1bea5511aae3af80c4bd
alexandrejablon <alexandre.jablon@gmail.com>
2014-02-27 17:46:19 +0100

    clean out the code a bit

--- content.js
+++ content.js
@@ -1,25 +1,24 @@
-exports.resume = {
+module.exports = {
   status: "doing an internship",
   education: {
     heading: 'Education',
     id: 'education',
     content: [
       {
-	key: '<b>Since 2011</b>',
-	content: '<p><b>MSc. (class of 2014)</b> at the Department of <b>Telecommunications, Services and Applications</b> at <b>INSA de Lyon</b> (National Institute of Technology of Lyon, France):</p><p><ul class="no-bullet-list"><li> &ndash; <b>Computer Science</b>: Operating Systems, Programming, Web,</li><li> &ndash; <b>Networks</b>: Computer Networking, Wireless Networking, P2P, ADSL, Sensor Networks,</li><li> &ndash; <b>Telecommunications</b>: Communication Theory, Signal Processing, RF Equipements,</li><li> &ndash; <b>Management</b>: Industrial, Technology and Information Systems.</li></ul></p><p>In 2012 – 2013, academic exchange year at <b>Tampere University of Technology</b> in Tampere, Finland.</p>'
+        key: '<b>Since 2011</b>',
+        content: '<p><b>MSc. (class of 2014)</b> at the Department of <b>Telecommunications, Services and Applications</b> at <b>INSA de Lyon</b> (National Institute of Technology of Lyon, France):</p><p><ul class="no-bullet-list"><li> &ndash; <b>Computer Science</b>: Operating Systems, Programming, Web,</li><li> &ndash; <b>Networks</b>: Computer Networking, Wireless Networking, P2P, ADSL, Sensor Networks,</li><li> &ndash; <b>Telecommunications</b>: Communication Theory, Signal Processing, RF Equipements,</li><li> &ndash; <b>Management</b>: Industrial, Technology and Information Systems.</li></ul></p><p>In 2012 – 2013, academic exchange year at <b>Tampere University of Technology</b> in Tampere, Finland.</p>'
       },
       {
-	key: '<b>From 2009 to 2011</b>',
-	content: '<p>Preparatory degree course at <i>INSA de Lyon</i>.</p>'
+        key: '<b>From 2009 to 2011</b>',
+        content: '<p>Preparatory degree course at <i>INSA de Lyon</i>.</p>'
       },
       {
-	key: '<b>Languages</b>',
-	content: '<p><ul class="no-bullet-list"><li> &ndash; <b>English</b>: good standard for reading, writing and speaking.</li><li> &ndash; French: native speaker.</li></ul></p>'
+        key: '<b>Languages</b>',
+        content: '<p><ul class="no-bullet-list"><li> &ndash; <b>English</b>: good standard for reading, writing and speaking.</li><li> &ndash; French: native speaker.</li></ul></p>'
       },
       {
-	key: '<b>Computer Science</b>',
-	content: '<p><b>Keywords and Languages: GNU/Linux, Agility, Python, REST, Node.js, Backbone.js, MongoDB, RethinkDB, Git. </b></p>'
-
+        key: '<b>Computer Science</b>',
+        content: '<p><b>Keywords and Languages: GNU/Linux, Agility, Python, REST, Node.js, Backbone.js, MongoDB, RethinkDB, Git. </b></p>'
       }
     ]
   },
@@ -28,20 +27,20 @@ exports.resume = {
     id: 'career',
     content: [
       {
-	key: '<b>From June 2013 to December 2013</b>',
-	content: '<p><b>Software Engineer Intern</b> at <b>Work4 Labs, The #1 Facebook Recruiting Solution</b>, in Paris, France.</p><p>The folks at Work4 Labs make a great Facebook application to enable companies to recruit talented people in a smarter way and they&#8217;re pioneering the world of Social Recruiting!</p><p>What I did: <ul class="no-bullet-list"><li> &ndash; Integration of our partner and client information into our database,</li><li> &ndash; Development and Implementation of specific options, tailored for our clients,</li><li> &ndash; Monitoring and Maintenance on existing integrations,</li><li> &ndash; Continuous Integration, Development and Maintenance on our backend platform and server.</li></ul></p><p>Technological platform: Python, MongoDB (+Twisted, Redis, Node.js, ...).</p>'
+        key: '<b>From June 2013 to December 2013</b>',
+        content: '<p><b>Software Engineer Intern</b> at <b>Work4 Labs, The #1 Facebook Recruiting Solution</b>, in Paris, France.</p><p>The folks at Work4 Labs make a great Facebook application to enable companies to recruit talented people in a smarter way and they&#8217;re pioneering the world of Social Recruiting!</p><p>What I did: <ul class="no-bullet-list"><li> &ndash; Integration of our partner and client information into our database,</li><li> &ndash; Development and Implementation of specific options, tailored for our clients,</li><li> &ndash; Monitoring and Maintenance on existing integrations,</li><li> &ndash; Continuous Integration, Development and Maintenance on our backend platform and server.</li></ul></p><p>Technological platform: Python, MongoDB (+Twisted, Redis, Node.js, ...).</p>'
       },
       {
-	key: '<b>July &ndash; August 2012</b>',
-	content: '<p><b>Developer Intern</b> at <b>Finsecur</b> (150 employees): I designed and coded a universal test bench program in Python for fire detection equipment.</p>'
+        key: '<b>July &ndash; August 2012</b>',
+        content: '<p><b>Developer Intern</b> at <b>Finsecur</b> (150 employees): I designed and coded a universal test bench program in Python for fire detection equipment.</p>'
       },
       {
-	key: '<b>July &ndash; August 2011</b>',
-	content: '<p><b>Developer Intern</b> at <b>Hutchinson</b> (turnover &euro; 3 billion, 27,000 employees): I designed and implemented a Python script and UI to launch computations on a computer, a virtual machine and a cluster.</p>'
+        key: '<b>July &ndash; August 2011</b>',
+        content: '<p><b>Developer Intern</b> at <b>Hutchinson</b> (turnover &euro; 3 billion, 27,000 employees): I designed and implemented a Python script and UI to launch computations on a computer, a virtual machine and a cluster.</p>'
       },
       {
-	key: '<b>July 2010</b>',
-	content: '<p>1-month internship at <b>La Pizza de Manosque</b> (50 employees): I worked on the pizza production line and designed and implemented a Computerized Maintenance Management System with MS Access.</p><p>Best Internship Report awarded by the <i>Association des Amis de l&#8217;Université de Lyon</i> (the Friends of University of Lyon).</p>'
+        key: '<b>July 2010</b>',
+        content: '<p>1-month internship at <b>La Pizza de Manosque</b> (50 employees): I worked on the pizza production line and designed and implemented a Computerized Maintenance Management System with MS Access.</p><p>Best Internship Report awarded by the <i>Association des Amis de l&#8217;Université de Lyon</i> (the Friends of University of Lyon).</p>'
       }
     ]
   },
@@ -50,8 +49,8 @@ exports.resume = {
     id: 'projects',
     content: [
       {
-	key: '<b>2010 to 2012</b>',
-	content: 'I worked in the communications committee of the 37th and 38th editions of the <i>24 Heures de l&#8217;INSA</i>, the biggest student festival in France which gathers 60,000 visitors in three days: I managed the website of the festival and I designed the leaflets, the T-Shirt and the cup logos. I worked as a bar manager during the events.'
+        key: '<b>2010 to 2012</b>',
+        content: 'I worked in the communications committee of the 37th and 38th editions of the <i>24 Heures de l&#8217;INSA</i>, the biggest student festival in France which gathers 60,000 visitors in three days: I managed the website of the festival and I designed the leaflets, the T-Shirt and the cup logos. I worked as a bar manager during the events.'
       }
     ],
   },
@@ -72,52 +71,52 @@ exports.resume = {
     id: 'places',
     content: [
       {
-	xcoord: 825,
-	ycoord: 320,
-	name: 'France',
-	type: 'home'
+        xcoord: 825,
+        ycoord: 320,
+        name: 'France',
+        type: 'home'
       },
       {
-	xcoord: 798,
-	ycoord: 253,
-	name: 'Scotland',
-	type: 'travel'
+        xcoord: 798,
+        ycoord: 253,
+        name: 'Scotland',
+        type: 'travel'
       },
       {
-	xcoord: 872,
-	ycoord: 302,
-	name: 'Germany',
-	type: 'travel'
+        xcoord: 872,
+        ycoord: 302,
+        name: 'Germany',
+        type: 'travel'
       },
       {
-	xcoord: 116,
-	ycoord: 407,
-	name: 'United States',
-	type: 'travel'
+        xcoord: 116,
+        ycoord: 407,
+        name: 'United States',
+        type: 'travel'
       },
       {
-	xcoord: 1009,
-	ycoord: 448,
-	name: 'Israel',
-	type: 'travel'
+        xcoord: 1009,
+        ycoord: 448,
+        name: 'Israel',
+        type: 'travel'
       },
       {
-	xcoord: 948,
-	ycoord: 194,
-	name: 'Finland',
-	type: 'home'
+        xcoord: 948,
+        ycoord: 194,
+        name: 'Finland',
+        type: 'home'
       },
       {
-	xcoord: 914,
-	ycoord: 220,
-	name: 'Sweden',
-	type: 'travel'
+        xcoord: 914,
+        ycoord: 220,
+        name: 'Sweden',
+        type: 'travel'
       },
       {
-	xcoord: 982,
-	ycoord: 78,
-	name: 'Norway',
-	type: 'travel'
+        xcoord: 982,
+        ycoord: 78,
+        name: 'Norway',
+        type: 'travel'
       }
     ]
   },
@@ -126,52 +125,52 @@ exports.resume = {
     id: 'currents',
     content: [
       {
-	name: 'node.js',
-	icon: 'node',
+        name: 'node.js',
+        icon: 'node',
       },
       {
-	name: 'backbone.js, underscore.js, jQuery',
-	icon: 'js_stuff',
+        name: 'backbone.js, underscore.js, jQuery',
+        icon: 'js_stuff',
       },
       {
-	name: 'Bootstrap',
-	icon: 'bootstrap',
+        name: 'Bootstrap',
+        icon: 'bootstrap',
       },
       {
-	name: 'Python',
-	icon: 'python',
+        name: 'Python',
+        icon: 'python',
       },
       {
-	name: 'Redis',
-	icon: 'redis',
+        name: 'Redis',
+        icon: 'redis',
       },
       {
-	name: 'MongoDB',
-	icon: 'mongo',
+        name: 'MongoDB',
+        icon: 'mongo',
       },
       {
-	name: 'RethinkDB',
-	icon: 'rethinkdb',
+        name: 'RethinkDB',
+        icon: 'rethinkdb',
       },
       {
-	name: 'git',
-	icon: 'git',
+        name: 'git',
+        icon: 'git',
       },
       {
-	name: 'The GIMP',
-	icon: 'gimp',
+        name: 'The GIMP',
+        icon: 'gimp',
       },
       {
-	name: 'Inkscape',
-	icon: 'inkscape',
+        name: 'Inkscape',
+        icon: 'inkscape',
       },
       {
-	name: 'Mac OS X',
-	icon: 'osx',
+        name: 'Mac OS X',
+        icon: 'osx',
       },
       {
-	name: 'Vim',
-	icon: 'vim',
+        name: 'Vim',
+        icon: 'vim',
       }
     ]
   },
@@ -184,4 +183,4 @@ exports.resume = {
     id: 'contact',
     email: 'alex@jablon.me'
   }
-}
+};
--- resume.js
+++ resume.js
@@ -1,84 +1,89 @@
+var crypto = require('crypto');
 var _ = require('underscore');
 var nodemailer = require("nodemailer");
 var content = require("./content.js");
-var crypto = require('crypto');
 var _pass = require ('./_pass.js');
 
 
 // Redis stuff
 // Redis is used to prevent mail flooding from a single ip address
 var redis = require("redis"),
-  client = redis.createClient('6379', 'localhost');
+    client = redis.createClient('6379', 'localhost');
 
 client.on("error", function (err) {
   console.log("Error " + err);
 });
 
-
-// Exports stuff
-exports.render = function (req, res) {
+var render = exports.render = function (req, res) {
   res.sendfile(__dirname + '/static/views/resume.html');
 };
 
-exports.get_resume = function (req, res) {
-  res.json (content.resume);
+var get_resume = exports.get_resume = function (req, res) {
+  res.json (content);
 };
 
-exports.blob = function (req, res) {
-  // I know md5 is crap, but it's good enough for this kind of usage
-  var mdsum = crypto.createHash('md5');
-  var ipHash = mdsum.digest('ascii');
-  client.set(ipHash, req.ip, 'EX', 60, 'NX', function (redisErr, redisRes) {
-    if (redisErr) {
+var blob = exports.blob = function (req, res) {
+  store_ip_hash (req.ip, function (redis_err, redis_res) {
+    if (redis_err) {
       res.send ({ 'status': 0 });
     } else {
-      if (redisRes === null) {
-	res.send ({ 'status': 3 });
+      if (redis_res === null) {
+        res.send ({ 'status': 3 });
       } else {
-	if (req.body.blobFrom && req.body.blobContent) {
-	  var smtpTransport = nodemailer.createTransport ("SMTP", {
-	    service: "Gmail",
-	    auth: {
-	      user: "alexandre.jablon.resume@gmail.com",
-	      pass: _pass
-	    }
-	  });
-
-	  var mailOptions = {
-	    from: "My Resume <alexandre.jablon.resume@gmail.com>",
-	    to: "alex+resume@jablon.me",
-	    subject: "Someone has sent you something from your resume",
-	    html: "<b>From:</b> " + req.body.blobFrom + "<br/><br/><b>Content:</b> <br/>" + req.body.blobContent
-	  }
-
-	  smtpTransport.sendMail (mailOptions, function (error, response) {
-	    if (error) {
-	      res.send ({ 'status': 0 });
-	    } else {
-	      res.send ({ 'status': 1 });
-	    }
-	  });
-	} else {
-	  res.send ({'status': 2});
-	}
+        var from = req.body.blobFrom, content = req.body.blobContent;
+        if (from && content) {
+          send_email ({'from': from, 'content': content}, function (error) {
+            if (error) {
+              res.send ({ 'status': 0 });
+            } else {
+              res.send ({ 'status': 1 });
+            }
+          });
+        } else {
+          res.send ({'status': 2});
+        }
       }
     }
   });
 };
 
-exports.robots = function (req, res) {
+var store_ip_hash = exports.store_ip_hash = function (ip, callback) {
+  var ip_h = crypto.createHash ('sha1').update (ip).digest ('hex').substring (0, 8);
+  client.set(ip_h, 'OK', 'EX', 60, 'NX', function (err, res) {
+    callback (err, res);
+  });
+};
+
+var send_email = exports.send_email = function (data, callback) {
+  var smtpTransport = nodemailer.createTransport ("SMTP", {
+    service: "Gmail",
+    auth: {
+      user: "alexandre.jablon.resume@gmail.com",
+      pass: _pass
+    }
+  });
+  var mailOptions = {
+    from: "My Resume <alexandre.jablon.resume@gmail.com>",
+    to: "alex+resume@jablon.me",
+    subject: "Someone has sent you something from your resume",
+    html: "<b>From:</b> " + data.from + "<br/><br/><b>Content:</b> <br/>" + data.content
+  }
+  smtpTransport.sendMail (mailOptions, callback);
+};
+
+var robots = exports.robots = function (req, res) {
   res.set ('Content-Type', 'text/plain');
   res.send (200, 'User-agent: Googlebot\nAllow: /\nUser-agent: *\nDisallow: /');
 };
 
-exports.fourofour = function (req, res) {
+var fourofour = exports.fourofour = function (req, res) {
   res.status (404).sendfile(__dirname + '/static/views/404.html');
 };
 
-exports.linkedin = function (req, res) {
+var linkedin = exports.linkedin = function (req, res) {
   res.redirect ('http://www.linkedin.com/in/alexjablon');
 }
 
-exports.github = function (req, res) {
+var github = exports.github = function (req, res) {
   res.redirect ('http://github.com/alexandrejablon');
 }
