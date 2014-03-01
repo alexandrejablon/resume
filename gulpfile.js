4f2c9a382292a97437c180695451c50fa590ddf5
alexjab <alexandre.jablon@gmail.com>
2014-03-01 23:46:38 +0100

    fix a gulp folder thingy

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
-var gulp = require('gulp');
+var gulp = require ('gulp');
 
-var uglify = require('gulp-uglify');
+var uglify = require ('gulp-uglify');
+var concat = require ('gulp-concat');
 
 gulp.task ('build', function(){
   gulp.src ('./static/js/resume.js')
   .pipe (uglify ())
-  .pipe (gulp.dest ('./static/js/resume.min.js'));
+  .pipe (concat ('resume.min.js'))
+  .pipe (gulp.dest ('./static/js/'));
 });
 
